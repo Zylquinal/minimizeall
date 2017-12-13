3a7d4a3f6207e551aad7d490d1469bcffa82747e
Charles Santos Silva <silva.charlessantos@gmail.com>
2017-12-13 16:12:37 +0000

    Atualiza ano

--- prefs.js
+++ prefs.js
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2013-2014, Charles Santos Silva (silva.charlessantos@gmail.com)
+  Copyright (c) 2013-2017, Charles Santos Silva (silva.charlessantos@gmail.com)
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are met:
